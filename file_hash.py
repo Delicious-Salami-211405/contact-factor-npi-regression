f3b339098fbf9de21e7b490fef159275fdd20627
Henry Wang <henrywang@HW-MBP.local>
2022-08-21 13:03:51 +0100

    Updated file hash

--- file_hash.py
+++ file_hash.py
@@ -2,7 +2,7 @@
 
 import hashlib
 
-filename = 'The effect of non-pharmaceutical interventions on COVID-19 contact factors vF.docx'
+filename = '[211405] The effect of non-pharmaceutical interventions on COVID-19 contact factors vF2.docx'
 with open(filename, 'rb') as f:
 	h = hashlib.sha256(f.read()).hexdigest()
 
